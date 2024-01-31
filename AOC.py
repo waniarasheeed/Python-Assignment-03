97eb9c75d4f3917eefca291ce1e5b48cab4a94e6
waniarasheeed <157637231+waniarasheeed@users.noreply.github.com>
2024-01-31 22:51:31 +0500

    Add files via upload
    #01-Area Of Circle Using Python
    #02-Average Of Two Numbers Using Python

---
+++ AOC.py
@@ -0,0 +1,4 @@
+#Calculate Area Of Circle
+radius = float(input("Enter the radius of the circle"))
+area = 3.142*radius*radius
+print("area of circle is:",area)
---
+++ ATN.py
@@ -0,0 +1,7 @@
+#To Calculate The Average Of Two Numbers
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+average = (num1 + num2) / 2
+print("average of two numbers = %.if" %average)
